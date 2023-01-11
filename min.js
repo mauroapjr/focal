7810a2107a926228cbb04dbba9a6c826116d4ccc
mauroapjr <mauroapjr@outlook.com>
2023-01-10 21:49:53 -0500

    create file and code

--- min.js
+++ min.js
@@ -1,7 +1,13 @@
-const min = function(numbers) {
-  // Write code here that returns the smallest value in numbers
-}
+const min = function (numbers) {
+  let smallvalue = numbers[0];
 
+  for (i = 0; i < numbers.length; i++)
+    if (numbers[i] < smallvalue) {
+      smallvalue = numbers[i];
+    }
+  return smallvalue;
+};
+// Write code here that returns the smallest value in numbers
 
 /*
  * This is some test runner code that's simply calling our min function defined
@@ -9,13 +15,29 @@ const min = function(numbers) {
  */
 
 const flightPrices = [1260, 490, 599, 1400, 820];
-console.log(`The cheapest flight amongst $1260, $490, $599, $1400 and $820 costs \$${min(flightPrices)}`);
+console.log(
+  `The cheapest flight amongst $1260, $490, $599, $1400 and $820 costs \$${min(
+    flightPrices
+  )}`
+);
 
 const golfScores = [-1, 3, 0, -4, 1, 4, -2];
-console.log(`The winning golf score amongst -1, 3, 0, -4, 1, 4 and -2 is ${min(golfScores)}`);
+console.log(
+  `The winning golf score amongst -1, 3, 0, -4, 1, 4 and -2 is ${min(
+    golfScores
+  )}`
+);
 
 const pageNumbers = [232];
-console.log(`The shortest book out of a list of single book with 232 pages has ${min(pageNumbers)} pages`);
+console.log(
+  `The shortest book out of a list of single book with 232 pages has ${min(
+    pageNumbers
+  )} pages`
+);
 
 const temperatures = [45, 10, -20, 0, 3, -20];
-console.log(`The coldest temperature amongst 45C, 10C, -20C, 0C, 3C and -20C is ${min(temperatures)}C`);
+console.log(
+  `The coldest temperature amongst 45C, 10C, -20C, 0C, 3C and -20C is ${min(
+    temperatures
+  )}C`
+);
